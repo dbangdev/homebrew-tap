a7a8659556c4e293c7bfcf01c50075d9622d01aa
linux_china <libing.chen@gmail.com>
2022-01-04 18:40:51 -0800

    Update to version 0.1.2

--- Formula/dbang.rb
+++ Formula/dbang.rb
@@ -1,9 +1,9 @@
 class Dbang < Formula
   desc 'CLI to manage Deno scripts'
   homepage 'https://dbang.dev'
-  version '0.1.1'
-  url 'https://github.com/dbangdev/dbang/releases/download/0.1.1/dbang-0.1.1-mac-x64.tar'
-  sha256 '972ffaa975ed339cd30b408dd742131b13fe0f22e1a149b65756fefb47ff0456'
+  version '0.1.2'
+  url 'https://github.com/dbangdev/dbang/releases/download/0.1.2/dbang-0.1.2-mac-x64.tar'
+  sha256 '7bfce53e2a91d9e4646478a721b7a2d81486b2c92a16ec24cfa237b31cb18813'
   license 'Apache-2.0'
 
   def install
