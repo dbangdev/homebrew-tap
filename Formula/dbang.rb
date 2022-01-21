3d4aec91f6059d1719dce4be46fcf80787a8cfbf
linux_china <libing.chen@gmail.com>
2022-01-21 11:49:51 -0800

    Update to version 0.1.7

--- Formula/dbang.rb
+++ Formula/dbang.rb
@@ -1,16 +1,16 @@
 class Dbang < Formula
   desc 'CLI to manage Deno scripts'
   homepage 'https://dbang.dev'
-  version '0.1.6'
+  version '0.1.7'
   license 'Apache-2.0'
 
   if OS.mac? && Hardware::CPU.intel?
-    url 'https://github.com/dbangdev/dbang/releases/download/0.1.6/dbang-0.1.6-x86_64-apple-darwin.tar'
-    sha256 '9a49cb8a862405f0f9d479053e3a535fd6bf3024f7ba289b6dfedefe3d8a8d27'
+    url 'https://github.com/dbangdev/dbang/releases/download/0.1.7/dbang-0.1.7-x86_64-apple-darwin.tar'
+    sha256 'df4c70ac5e20f42746544e4c841226e0351d72dccb7a963fe482778a0c1bba4a'
   end
   if OS.mac? && Hardware::CPU.arm?
-    url 'https://github.com/dbangdev/dbang/releases/download/0.1.6/dbang-0.1.6-aarch64-apple-darwin.tar'
-    sha256 'd1600e7286d4878ecc72543797fa3d331a8af64e4256cc262e4dc1206f4db8d1'
+    url 'https://github.com/dbangdev/dbang/releases/download/0.1.7/dbang-0.1.7-aarch64-apple-darwin.tar'
+    sha256 '043073b7f2dd5b2cbca6a62c74d4b75f4c646b0f52f564a15f2a1481415a3331'
   end
 
   def install
