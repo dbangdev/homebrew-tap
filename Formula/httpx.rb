a6a74e6e96ff9d8e5df0e2cbb827252c6585436f
linux_china <libing.chen@gmail.com>
2022-01-29 17:12:38 -0800

    Update to version 0.4.2

--- Formula/httpx.rb
+++ Formula/httpx.rb
@@ -1,18 +1,18 @@
 class Httpx < Formula
   desc 'httpx - CLI to run HTTP file'
   homepage 'https://github.com/linux-china/httpx'
-  version '0.3.0'
+  version '0.4.2'
   license 'Apache-2.0'
 
   depends_on 'grpcurl'
 
   if OS.mac? && Hardware::CPU.intel?
-    url 'https://github.com/linux-china/httpx/releases/download/0.3.0/httpx-0.3.0-x86_64-apple-darwin.tar'
-    sha256 '32d0d2c49c8c583f83c4eeca544d8e192ebccd9deb388d924b37e39d61ae1982'
+    url 'https://github.com/linux-china/httpx/releases/download/0.4.2/httpx-0.4.2-x86_64-apple-darwin.tar'
+    sha256 '1d40af44967c339cfd6b7bb9de945ff0f78915230ac88f2967683ed7f4019b75'
   end
   if OS.mac? && Hardware::CPU.arm?
-    url 'https://github.com/linux-china/httpx/releases/download/0.3.0/httpx-0.3.0-x86_64-apple-darwin.tar'
-    sha256 '32d0d2c49c8c583f83c4eeca544d8e192ebccd9deb388d924b37e39d61ae1982'
+    url 'https://github.com/linux-china/httpx/releases/download/0.4.2/httpx-0.4.2-x86_64-apple-darwin.tar'
+    sha256 '1d40af44967c339cfd6b7bb9de945ff0f78915230ac88f2967683ed7f4019b75'
   end
 
   def install
